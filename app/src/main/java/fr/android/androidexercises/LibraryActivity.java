f5faef9c7da27a54f0cacb643ddede7fee316a80
francois <francoisadam2@gmail.com>
2016-12-14 09:09:53 +0100

    images

--- app/src/main/java/fr/android/androidexercises/LibraryActivity.java
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.java
@@ -2,6 +2,9 @@ package fr.android.androidexercises;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class LibraryActivity extends AppCompatActivity {
 
@@ -9,9 +12,15 @@ public class LibraryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_library);
+        ImageView downloadedImageView = (ImageView) findViewById(R.id.downloadImageView);
 
         // TODO Get image view and load image form URL
         // http://img3.wikia.nocookie.net/__cb20120317101541/harrypotter/images/3/37/Gryffindor_Crest.jpg
+
+        Glide.with(this)
+                .load("http://img3.wikia.nocookie.net/__cb20120317101541/harrypotter/images/3/37/Gryffindor_Crest.jpg")
+                .centerCrop()
+                .into(downloadedImageView);
     }
 
 }
