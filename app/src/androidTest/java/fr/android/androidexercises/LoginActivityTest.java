a55c5250ec9b6f4fae43502f236abe469187bdbb
francois <francoisadam2@gmail.com>
2016-12-14 15:20:09 +0100

    Tests Fonctionnels

--- app/src/androidTest/java/fr/android/androidexercises/LoginActivityTest.java
+++ app/src/androidTest/java/fr/android/androidexercises/LoginActivityTest.java
@@ -1,5 +1,9 @@
 package fr.android.androidexercises;
 
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.action.ViewActions;
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.test.ActivityInstrumentationTestCase2;
 
 public class LoginActivityTest extends ActivityInstrumentationTestCase2<LoginActivity> {
@@ -18,6 +22,14 @@ public class LoginActivityTest extends ActivityInstrumentationTestCase2<LoginAct
     public void takeScreenshot(String name) {
         //Spoon.screenshot(getCurrentActivity(), name);
     }
+
+    public void testLogin() throws Exception {
+        Espresso.onView(ViewMatchers.withId(R.id.usernameEdit)).perform(ViewActions.typeText("b@xebia.fr"));
+        Espresso.onView(ViewMatchers.withId(R.id.passwordEdit)).perform(ViewActions.typeText("password"), ViewActions.closeSoftKeyboard());
+        Espresso.onView(ViewMatchers.withId(R.id.loginButton)).perform(ViewActions.click());
+        Espresso.onView(ViewMatchers.withId(R.id.loggedText)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+    }
+
 
 //
 //
